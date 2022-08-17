49a27a5e0207446e0c8f4c2e098fd4494995c200
Joachim Lehmann <joa.lehmann@web.de>
2022-08-17 14:35:58 +0200

    Add isChecked to items in useStore

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -4,11 +4,12 @@ import create from 'zustand';
 const useStore = create(set => {
 	return {
 		items: [
-			{item: 'Zelt', id: nanoid()},
-			{item: 'Grill', id: nanoid()},
-			{item: 'Wein', id: nanoid()},
-			{item: 'noch mehr Wein', id: nanoid()},
+			{item: 'Zelt', id: nanoid(), isChecked: true},
+			{item: 'Grill', id: nanoid(), isChecked: false},
+			{item: 'Wein', id: nanoid(), isChecked: false},
+			{item: 'noch mehr Wein', id: nanoid(), isChecked: false},
 		],
+
 		addItem: item => {
 			set(state => {
 				return {items: [...state.items, {item, id: nanoid()}]};
