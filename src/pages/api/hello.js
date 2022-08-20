5bf776f754740c18d777407344e9652022a27216
Joachim Lehmann <joa.lehmann@web.de>
2022-08-20 20:41:12 +0200

    Add API

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,3 +1,16 @@
+import Item from '../../models/itemModel';
+import {dbConnect} from '../../src/dbConnect';
+
 export default async function handler(request, response) {
-	response.status(200).json({name: 'John Doe'});
+	await dbConnect();
+
+	if (request.method === 'POST') {
+		const data = JSON.parse(request.body);
+
+		await Item.create(data);
+
+		response.status(200).json({
+			message: 'item created',
+		});
+	}
 }
