659da5873c48e6c970048ae5c0305c83073ba95d
Joachim Lehmann <joa.lehmann@web.de>
2022-09-02 21:38:54 +0200

    Create Category-Card File

---
+++ src/components/StyledCatergoryCard.js
@@ -0,0 +1,15 @@
+export default function StyledCategoryCard() {
+	return (
+		<article>
+			<h3>Mountainbike</h3>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+			<button></button>
+			<button></button>
+		</article>
+	);
+}
