d040788866d8f56f814f93e0582f64238da2d5b6
Joachim Lehmann <joa.lehmann@web.de>
2022-09-02 23:55:10 +0200

    Update Category-Card

--- src/components/StyledCatergoryCard.js
+++ src/components/StyledCatergoryCard.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styled from 'styled-components';
 
 import {Trashcan} from './Buttons/Button';
@@ -5,8 +6,15 @@ import {Trashcan} from './Buttons/Button';
 export default function StyledCategoryCard() {
 	return (
 		<Card>
-			<button>bookmark</button>
-			<h3>Mountainbike</h3>
+			<Bookmark>
+				<Image
+					src="/bookmark.svg"
+					alt="An SVG of an bookmark icon"
+					width="24px"
+					height="24px"
+				/>
+			</Bookmark>
+			<CategoryName>Mountainbike</CategoryName>
 			<button>Add items</button>
 			<Trashcan />
 		</Card>
@@ -19,9 +27,22 @@ const Card = styled.article`
 	align-items: center;
 	width: 200px;
 	height: fit-content;
-	padding: 10px;
 	border-width: 1px;
 	border-style: solid;
 	border-radius: 20px;
 	border-color: black;
+`;
+
+const CategoryName = styled.h3`
+	margin: 5px;
+`;
+
+const Bookmark = styled.button`
+	align-self: flex-end;
+	margin-top: 5px;
+	border: none;
+	background-color: transparent;
+	&:hover {
+		cursor: pointer;
+	}
 `;
