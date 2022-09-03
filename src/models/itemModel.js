90f767b025ce0008d7eecee4f450011702ebd35f
Joachim Lehmann <joa.lehmann@web.de>
2022-09-03 15:28:06 +0200

    Update itemsModel.js with Category

--- src/models/itemModel.js
+++ src/models/itemModel.js
@@ -4,6 +4,7 @@ const {Schema} = mongoose;
 const cardSchema = new Schema({
 	item: {type: String, required: true},
 	isChecked: {type: Boolean, default: false},
+	category: {type: Schema.Types.ObjectId, ref: 'Category'},
 });
 
 const Item = mongoose?.models?.Item || mongoose.model('Item', cardSchema);
